f535a3b93c7d2e6d2fcdcd7dcaff16e913fd2b81
JulieBourelDrapier <juliebourel@hotmail.com>
2023-05-04 17:03:55 +0200

    Rec before deletion (2essai JS)

--- assets/script.js
+++ assets/script.js
@@ -17,16 +17,61 @@ const slides = [
 	}
 ]
 // ajout des EventListener sur les flèches 
-function onButtonClick (event) {
-	console.log(event.currentTarget)//pour savoir sur quel flèche on a cliqué
-}
-document.querySelectorAll('button').forEach(button => {
-	button.addEventlistener('click',onButtonClick) 
-})
+//function onButtonClick (event) {
+	//console.log(event.currentTarget)//pour savoir sur quel flèche on a cliqué
+//}
+//document.querySelectorAll('button').forEach(button => {
+	//button.addEventlistener('click',onButtonClick) 
+//})
 
 //autre essai flèche par flèche 
 //left arrow click
-arrow_left.addEventlistener('click', function(){ 
+let sliderImages =
+document.querySelectorAll('banner'),
+	arrowLeft = document.querySelector('.arrow_left'),
+	arrowRight = document.querySelector('.arrow_right'),
+	current = 0;
+
+//clear all images
+function reset(){
+	for(let i = 0; i < sliderImages.length; i ++){
+		sliderImages[i].style.display = 'none';
+	}
+}
+//initialise slider 
+function startSlide(){
+	reset();
+	sliderImages[0].style.display = 'block';
+}
+
+//show previous
+function slideleft(){
+	reset();
+	sliderImages[current -1].style.display = 'block';
+}
+
+//show next 
+function slideRight(){
+	reset();
+	sliderImages[current + 1].style.display = 'block';
+	current ++;
+}
+
+//left arrow click
+arrowLeft.addEventlistener('click', function(){ 
 	if (current === 0){
-		current = 
+		current = sliderImages.lengh;
+	}
+	slideLeft();
+});
+
+//right arrow click
+arrowRight.addEventlistener('click', function(){ 
+	if (current === sliderImages.lenght -1){
+		current = -1;
+	}
+	slideRight();
+});
+
+startSlide();
 
