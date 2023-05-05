f43e8a9a8ab0c88a728454532e6196cbe099828d
JulieBourelDrapier <juliebourel@hotmail.com>
2023-05-05 17:41:22 +0200

    delete Hello world

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,37 @@
-// annonce des variables 
+// annonce variable
 let slideBySlide = 0; // permer de savoir sur quelle slide je me trouve
-let 
 
+//pour faire défiler le carrousel j'utiliserai la fonction suivante
+function nextSlide (){
+}
 
-const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
-// ajout des EventListener sur les flèches 
+//DOM
+window.onload = () => {
+	const banner = document.querySelector('.banner');
+	//partie présente dans le JS 
+	const slides = [
+		{
+			"image":"slide1.jpg",
+			"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		},
+		{
+			"image":"slide2.jpg",
+			"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		},
+		{
+			"image":"slide3.jpg",
+			"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		},
+		{
+			"image":"slide4.png",
+			"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		}
+	]
+	// ajout des EventListener sur les flèches 
+	let leftArrow = document.querySelector('.arrow_left');//pr la récup
+	leftArrow.addEventListener('click', nextSlide)// clic
+}
 
 // raccourci commentaire sur une ligne = Ctrl + /
 
 
-// info.JS
-let message = 'Hello world';
-alert(message);
